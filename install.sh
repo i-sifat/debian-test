db12bba491e52dcc39212487208d01bdcef6b416
Murshed Alam Sifat <142529114+i-sifat@users.noreply.github.com>
2023-08-25 22:13:54 +0600

    # Check if the script is being run as root

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root"
+    exit 1
+fi
+
 # Various utilities
 sudo apt install -y gnome-icon-theme
 
